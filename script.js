33630de1c3b810a88e99915ed46d15f19aedba02
Dominik Vdovjak <dominik.vdovjak@gmail.com>
2024-01-04 23:37:15 +0100

    Adding content

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const hamburger = document.querySelector(".mobile_menu");
 const naviganion = document.querySelector(".navigation");
+var mItems = document.querySelectorAll(".btn_ul");
 
 hamburger.addEventListener("click", () => {
     naviganion.classList.toggle("active");
     hamburger.classList.toggle("active");
 })
-
+for(var i = 0 ; i < mItems.length ; i++){
+    mItems[i].addEventListener("click", () => {
+    naviganion.classList.toggle("active");
+    hamburger.classList.toggle("active");    
+    })
+}
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
@@ -17,3 +23,23 @@ const observer = new IntersectionObserver((entries) => {
 })
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+// Collapsible
+var collapsible = document.getElementsByClassName("Collapsible")
+for(var i = 0 ; i < collapsible.length ; i++){
+    collapsible[i].addEventListener("click", function() {
+        var collItems = this.parentNode.firstChild;
+        while (collCurrent) {
+            if (collItems.nodeType === 1 && collItems !== this){
+                collItems.classList.remove("large")
+            }
+            collCurrent = collCurrent.nextSibling;
+        }
+        if(this.classList.contains("large")){
+            this.classList.remove("large");
+        } else {
+            this.classList.toggle("large");
+        }
+    })
+}
